df04ffcc57a7a265fd527144d0c59e53dad1974b
Michael Kuehl <hello@txs.vc>
2023-04-05 12:52:44 +0200

    basic service to listen on Kafka topics

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/redhat-partner-ecosystem/shadowcar
+
+go 1.18
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/prometheus/client_golang v1.12.1
+	github.com/txsvc/goreq v1.0.1
+	github.com/txsvc/stdlib/v2 v2.2.0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ svc/simulator/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+
+	"github.com/txsvc/stdlib/v2/env"
+
+	"github.com/redhat-partner-ecosystem/shadowcar/internal"
+)
+
+var (
+	kc *kafka.Consumer
+	//kp *kafka.Producer
+)
+
+func init() {
+
+	clientID := env.GetString("client_id", "simulator")
+	groupID := env.GetString("group_id", "shadowcar")
+	autoOffset := env.GetString("auto_offset", "end") // smallest, earliest, beginning, largest, latest, end
+
+	// kafka setup
+	kafkaService := env.GetString("kafka_service", "")
+	if kafkaService == "" {
+		panic(fmt.Errorf("missing env 'kafka_service'"))
+	}
+	kafkaServicePort := env.GetString("kafka_service_port", "9092")
+	kafkaServer := fmt.Sprintf("%s:%s", kafkaService, kafkaServicePort)
+
+	// https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+	_kc, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":       kafkaServer,
+		"client.id":               clientID,
+		"group.id":                groupID,
+		"connections.max.idle.ms": 0,
+		"auto.offset.reset":       autoOffset,
+		"broker.address.family":   "v4",
+	})
+	if err != nil {
+		panic(err)
+	}
+	kc = _kc
+
+	/*
+		_kp, err := kafka.NewProducer(&kafka.ConfigMap{
+			"bootstrap.servers": kafkaServer,
+		})
+		if err != nil {
+			panic(err)
+		}
+		kp = _kp
+	*/
+
+	// prometheus endpoint setup
+	internal.StartPrometheusListener()
+}
+
+func main() {
+	sourceTopic := env.GetString("source_topic", "")
+
+	// metrics collectors
+	opsTxProcessed := promauto.NewCounter(prometheus.CounterOpts{
+		Name: "kafka_listener_events",
+		Help: "The number of processed events",
+	})
+
+	// create a responder for delivery notifications
+	evts := make(chan kafka.Event, 1000) // FIXME not sure if such a number is needed ...
+	go func() {
+		fmt.Println(" --> waiting for events")
+		for {
+			e := <-evts
+
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf(" --> delivery error: %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// subscribe
+	err := kc.SubscribeTopics(strings.Split(sourceTopic, ","), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf(" --> listening on topic(s) '%s'\n", sourceTopic)
+
+	for {
+		msg, err := kc.ReadMessage(-1)
+
+		if err == nil {
+			fmt.Printf("%s,%s: %s\n", msg.Timestamp.Format(time.RFC3339), msg.TopicPartition, string(msg.Value))
+
+			/*
+				// back to a json string
+				data, err := json.Marshal(tx)
+				if err != nil {
+					// do something
+				}
+
+				// send to the next destination
+				err = kp.Produce(&kafka.Message{
+					TopicPartition: kafka.TopicPartition{
+						Topic:     &targetTopic,
+						Partition: kafka.PartitionAny,
+					},
+					Value: data,
+				}, evts)
+				if err != nil {
+					fmt.Printf(" --> producer error: %v\n", err)
+				}
+			*/
+
+			// metrics
+			opsTxProcessed.Inc()
+
+		} else {
+			// The client will automatically try to recover from all errors.
+			fmt.Printf(" --> consumer error: %v (%v)\n", err, msg)
+		}
+	}
+}
